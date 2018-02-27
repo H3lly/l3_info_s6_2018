35b4fc4256c6b2427dbb259766f11c541142eea5
Camille Aussignac <camaussignac@emi.u-bordeaux1.fr>
2018-02-27 15:30:29 +0100

    oizjoij

--- TAP/td01-tsp_exact/tsp.c
+++ TAP/td01-tsp_exact/tsp.c
@@ -307,7 +307,7 @@ int main(int argc, char *argv[]) {
 	bool need_redraw = true;
 	bool wait_event = true;
 
-	int n = 50;
+	int n = 5;
 	point *V = generatePoints(n, width, height);
 	int *P = malloc(n * sizeof(int));
 	for(int i = 0; i < n; i++) P[i] = i; // première permutation
